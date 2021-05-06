3cf89778413ed9266bfe513244aa5bbdfc0d5b7f
Brendan Ryan <brendantipp@gmail.com>
2021-05-06 20:55:41 +0100

    initial upload sorting

---
+++ sorting_fucntions.py
@@ -0,0 +1,30 @@
+#The five sorting fucntions i have selected are as follows
+
+
+
+# Bubble sort - simple comparison based sort
+
+
+
+
+
+
+
+## Merge sort - efficient comparison based sort
+
+
+
+
+# Counting sort - a non comparison sort 
+
+
+
+
+#Insertion sort - a simple comparison based sort
+
+
+
+
+
+
+# Heap sort 
