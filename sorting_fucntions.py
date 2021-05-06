d3a08f7a073dac5392e68d115f70b67b56546b00
Brendan Ryan <brendantipp@gmail.com>
2021-05-06 21:00:43 +0100

    added bubble sort

--- sorting_fucntions.py
+++ sorting_fucntions.py
@@ -2,9 +2,25 @@
 
 
 
+
+
 # Bubble sort - simple comparison based sort
+#code as per lectures
 
+def sort(arr):
+    n = len(arr)
+    for outer in range(n-1, 0, -1):
+        for inner in range(0,outer,1):
+            if arr[inner] > arr[inner+1]:
+                temp = arr[inner]
+                arr[inner] = arr[inner+1]
+                arr[inner+1] = temp
+    print(arr)
+    #return arr 
 
+#print(arr)
+arr = [12,3,6]
+sort(arr)
 
 
 
@@ -15,7 +31,12 @@
 
 
 
+
+
 # Counting sort - a non comparison sort 
+
+
+
 
 
 
@@ -27,4 +48,8 @@
 
 
 
+
+
 # Heap sort 
+
+
