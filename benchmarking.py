c90b6504ef044b9688fab28b91e2e6423afb5956
Brendan Ryan <brendantipp@gmail.com>
2021-05-07 18:42:19 +0100

    wrapping time in a function

--- benchmarking.py
+++ benchmarking.py
@@ -5,28 +5,17 @@ import sorting_fucntions as sf
 from random import randint
 
 
+
 def random_array(n):
     array = []
-    for _ in range(0,n,1):
+    for i in range(0,n,1):
         array.append(randint(0 , 100))
+        
     return array
 
-array = random_array(100000)
+print(random_array(100))
 
-#log the start time in seconds
-start_time = time.time()
-
-#call the function that you want to benchmark
-
-
-#log the end time in seconds
-end_time = time.time()
-
-
-#calculate the elapsed time in seconds
-time_elapsed = end_time - start_time
-
-## example below note time has already been imported
+#input_array = random_array(100)
 
 num_runs =  10 #number of times to test the function
 results = [] # array to stroe results for each test
@@ -34,22 +23,31 @@ results = [] # array to stroe results for each test
 
 #benchmarking the function
 
+def timer(algorithm,array):
+    
+    for r in range(num_runs):
+        random_array(100)
+        # log the start time in seconds
+        start_time = time.time()
+        
+        # call the function that you want to benchmark
+
+        algorithm(array)
+       
+
+        #sf.merge_sort(array)
+
+        #log the end time in seconds
+        end_time = time.time()
+        
+        #calculate the elapsed time
+        time_elapsed = end_time - start_time
+        
+        results.append(time_elapsed)
+        print("run",r)
+        print(time_elapsed)
 
 
-for r in range(num_runs):
-    # log the start time in seconds
-    start_time = time.time()
-    
-    # call the function that you want to benchmark
-    
-    sf.merge_sort(array)
 
-    #log the end time in seconds
-    end_time = time.time()
-    
-    #calculate the elapsed time
-    time_elapsed = end_time - start_time
-    
-    results.append(time_elapsed)
+timer(sf.bubble_sort,array)
 
-    print(time_elapsed)
